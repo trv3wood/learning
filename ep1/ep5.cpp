bf764e81fe2ae72cb87f33e0f03599551b43372d
trv3wood <1841369469@qq.com>
2023-10-23 21:10:29 +0800

    add comments to ep5.cpp

--- ep1/ep5.cpp
+++ ep1/ep5.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
-//过了！！！！！  
+
+// This program prints a pyramid of characters based on user input
+// The first input is a character to be printed
+// The second input is the number of rows in the pyramid
+
 int main() {
     char ch;
     int n;
-    while (cin >> ch >> n) {
-        for(int j = 1 ; j<=n ;++j){
-            for(int i = 1; i<=j;i++){
-                for (int x = 1; x <= n - i; x++) {
+    while (cin >> ch >> n) { // read input character and number of rows and pattern
+        for(int j = 1 ; j<=n ;++j) { // loop through each pattern 
+            for(int i = 1; i <= j; i++){ // loop through each row
+                for (int x = 1; x <= n - i; x++) { // print spaces before the character
                     cout << " ";
                 }
-                for (int x = 1; x <= 2 * i - 1; x++) {
+                for (int x = 1; x <= 2 * i - 1; x++) { // print the character
                     cout << ch;
                 }
-            cout<<endl;
+            cout<<endl; // move to the next line after each row is printed
             }
         }
     }
