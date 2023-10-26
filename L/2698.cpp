914fef17a0b1b641faa472f08bba9bd07968184b
trv3wood <1841369469@qq.com>
2023-10-26 15:18:02 +0800

    leetcode打卡

---
+++ L/2698.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+//给你一个正整数 n ，请你返回 n 的 惩罚数 。
+
+//n 的 惩罚数 定义为所有满足以下条件 i 的数的平方和：
+
+//1 <= i <= n
+//i * i 的十进制表示的字符串可以分割成若干连续子字符串，且这些子字符串对应的整数值之和等于 i 。
+class Solution {
+public:
+    int punishmentNumber(int n) {
+    }
+};
+
+int main() {
+    Solution s;
+    std::cout << s.punishmentNumber(10) << std::endl;
+    return 0;
+}
