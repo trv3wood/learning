1c3c317b68e6bf596b378dbb5835564b3d02fa95
trv3wood <1841369469@qq.com>
2023-11-01 22:16:23 +0800

    全部通过

--- lab_class2_midtermExam/G.cpp
+++ lab_class2_midtermExam/G.cpp
@@ -16,7 +16,7 @@ using namespace std;
 bool isPrime(int x) {
     if(x == 1)
         return false;
-    for(int i = 2; i < x; ++i)
+    for(int i = 2; i * i <= x; ++i)
         if(x % i == 0)
             return false;
     return true;
