208b6dc0a65d27f8d4b74318c948849f16381f80
trv3wood <1841369469@qq.com>
2023-11-14 17:47:47 +0800

    I.ko

--- assignment/assignment5/I.cpp
+++ assignment/assignment5/I.cpp
@@ -1,4 +1,7 @@
-/*关于自然常数e有许多有趣的结果，例如e等于表达式(1+1/n)^n在n趋于无穷时的极限，又例如e等于表达式1+1/1!+1/2!+…+1/n!在n趋于无穷时的极限（n!为n的阶乘，即n*(n-1)*(n-2)…*3*2*1），请根据输入的正整数n，计算上述两种表达式的比值，即((1+1/n)^n)/( 1+1/1!+1/2!+…+1/n!)，比值精确到4位小数。
+/*关于自然常数e有许多有趣的结果，
+例如e等于表达式(1+1/n)^n在n趋于无穷时的极限，
+又例如e等于表达式1+1/1!+1/2!+…+1/n!在n趋于无穷时的极限（n!为n的阶乘，即n*(n-1)*(n-2)…*3*2*1），
+请根据输入的正整数n，计算上述两种表达式的比值，即((1+1/n)^n)/( 1+1/1!+1/2!+…+1/n!)，比值精确到4位小数。
 输入
 多组输入，每行一个正整数n
 输出
@@ -23,3 +26,30 @@
 0.9950
 0.9995
 1.0000*/
+#include <bits/stdc++.h>
+using namespace std;
+
+long double e1(int n) {
+    long double res = 1.0;
+    for(int i = 0; i < n; i++)
+        res *= 1.0 + 1.0 / n;
+    return res;
+}
+
+long double e2(int n) {
+    long double res = 1.0;
+    long double nnn = 1.0;
+    for(int i = 1; i <= n; i++) {
+        nnn *= (1.0 / i);
+        res += nnn;
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    while(cin >> n) {
+        // cout << e1(n) << " " << e2(n) << endl;
+        cout << fixed << setprecision(4) << (e1(n) / e2(n)) << endl;
+    }
+}
