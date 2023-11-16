f6c8f15d4f35dc573ffa4a6964a0eff8b5021d78
trv3wood <1841369469@qq.com>
2023-11-16 17:01:22 +0800

    11周习题解决

--- assignment/assignment5/L.cpp
+++ assignment/assignment5/L.cpp
@@ -1,6 +1,10 @@
-/*恺撒密码是最简单的加密技术之一，其规则如下：指定一个整数-26<n<26，则明文中的大写英文字母将被加密为在其在字母表中向右平移n位对应的大写字母，其他字符不参与加解密，这种平移是循环的，例如Z向右平移1位得到A，n小于0则代表循环左移，若要解密密文，只需对密文进行反方向操作。示例：n=3，ZOO_QUAD将被加密为CRR_TXDG。
+/*恺撒密码是最简单的加密技术之一，其规则如下：指定一个整数-26<n<26，
+则明文中的大写英文字母将被加密为在其在字母表中向右平移n位对应的大写字母，
+其他字符不参与加解密，这种平移是循环的，例如Z向右平移1位得到A，n小于0则代表循环左移，
+若要解密密文，只需对密文进行反方向操作。示例：n=3，ZOO_QUAD将被加密为CRR_TXDG。
 输入
-多组输入，每行包括一个字符作为操作指令，一个整数n和一个字符串，当操作指令为’e’时，使用整数n进行加密操作，为’d’时则进行解密操作。
+多组输入，每行包括一个字符作为操作指令，一个整数n和一个字符串，当操作指令为’e’时，
+使用整数n进行加密操作，为’d’时则进行解密操作。
 输出
 按行输出对应输入的加解密结果
 样例输入 Copy
@@ -11,3 +15,50 @@ e 5 THE_QUICK_BROWN_FOX_JUMPS_OVER_THE_LAZY_DOG
 CRR_TXDG
 ZOO_QUAD
 YMJ_VZNHP_GWTBS_KTC_OZRUX_TAJW_YMJ_QFED_ITL*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void encrypt(int move, string& Texts) {
+    for(char c : Texts) {
+        if(c >= 'A' && c <= 'Z') {
+            c = (char)('A' + ((toascii(c) - 'A' + move) % 26));
+            cout << c;
+        } else if(c >= 'a' && c <= 'z') {
+            c = (char)('A' + ((toascii(c) - 'a' + move) % 26));
+            cout << c;
+        }
+        else { 
+            cout << c;
+        }
+    }
+    cout << endl;
+}
+
+void decrypt(int remove, string& Texts) {
+    for(char c : Texts) {
+        if(c >= 'A' && c <= 'Z') {
+            c = (char)('A' + ((toascii(c) - 'A' + remove) % 26));
+            cout << c;
+        } else if(c >= 'a' && c <= 'z') {
+            c = (char)('A' + ((toascii(c) - 'a' + remove) % 26));
+            cout << c;
+        } else {
+            cout << c;
+        }
+    }
+    cout << endl;   
+}
+
+int main() {
+    char command;
+    int move;
+    string plainTexts;
+    while(cin >> command >> move >> plainTexts) {
+        if(command == 'e')
+            encrypt(move, plainTexts);
+        else if(command == 'd') {
+            int remove = 26 - move;
+            decrypt(remove, plainTexts);
+        }
+    }
+}
