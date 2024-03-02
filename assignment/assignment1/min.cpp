718f1a7c2b7749b51f789cd157e0aa7c31b1c815
Zhu Yebin <146869511+Rubenzyb@users.noreply.github.com>
2024-03-02 18:13:17 +0800

    测试

--- assignment/assignment1/min.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main(){
-    int a,b,c;
-    while (std::cin>>a>>b>>c)//取最小的整数
-    {
-        std::cout<<(a<=b?(a<=c?a:c):(b<=c?b:c))<<std::endl;
-    }
-}
--- assignment/assignment3/oj3I/CMakeFiles/oj3I.dir/Fabricate.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/oj3I.dir/Fabricate.cpp.o: \
-  /mnt/d/code\ vsc/cpp/learning/oj3/oj3I/Fabricate.cpp \
-  /mnt/d/code\ vsc/cpp/learning/oj3/oj3I/combinatorics.h
